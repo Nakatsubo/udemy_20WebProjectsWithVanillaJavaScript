4315de6b81fa024ca435bad1ab427ea7758b2a99
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-02-27 20:10:25 +0900

    add simple validation

--- FormValidator_IntroProject/src/assets/js/index.js
+++ FormValidator_IntroProject/src/assets/js/index.js
@@ -1,3 +1,46 @@
 import  '../scss/style.scss';
 
-console.log('Hello, World');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+const showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}  
+
+// Event Listeners
+form.addEventListener('submit', (e) => {
+  // event.preventDefaultメソッドは、submitイベントの発生元であるフォームが持つデフォルトの動作をキャンセルするメソッドです。
+  e.preventDefault();
+
+  if (username.value === '') showError(username, 'Username is required');
+  else showSuccess(username);
+
+  if (email.value === '') showError(email, 'Email is required');
+  else if (!isValidEmail(email.value)) showError(email, 'Email is not valid');
+  else showSuccess(email);
+
+  if (password.value === '') showError(password, 'Password is required');
+  else showSuccess(password);
+
+  if (password2.value === '') showError(password2, 'Password2 is required');
+  else showSuccess(password2);
+});
