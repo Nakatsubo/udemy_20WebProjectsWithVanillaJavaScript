445ed874d98218467a7a36623fe8c347f6eed0c4
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-04-02 22:27:35 +0900

    set Video Progress Bar & Timestamp

--- CustomVideoPlayer_HTML5_VideoAPI/src/assets/js/index.js
+++ CustomVideoPlayer_HTML5_VideoAPI/src/assets/js/index.js
@@ -20,12 +20,23 @@ const updatePlayIcon = () => {
 
 // Update progress & timestamp
 const updateProgress = () => {
-  return true;
+  // console.log(video.currentTime);
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minites
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) mins = '0' + String(mins);
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) secs = '0' + String(secs);
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 const setVideoProgress = () => {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
