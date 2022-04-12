0f8d41195367495119bb8df52230a32d54b0c45e
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-04-12 22:18:18 +0900

    add @babel/plugin-transform-runtime Plugin

--- DOMArrayMethods_forEach_map_sort_filter_reduce/src/assets/js/index.js
+++ DOMArrayMethods_forEach_map_sort_filter_reduce/src/assets/js/index.js
@@ -1,3 +1,22 @@
 import  '../scss/style.scss';
 
-console.log('Hello, World');
+const main                = document.getElementById('main');
+const addUserBtn          = document.getElementById('add-user');
+const dubleBtn            = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn             = document.getElementById('sort');
+const calculateWealthBtn  = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser()
+
+// fetch random user and money
+async function getRandomUser() {
+  const res  = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  console.log(data);
+}
--- DOMArrayMethods_forEach_map_sort_filter_reduce/webpack.config.js
+++ DOMArrayMethods_forEach_map_sort_filter_reduce/webpack.config.js
@@ -87,6 +87,7 @@ module.exports = {
               presets: [
                 "@babel/preset-env",
               ],
+              plugins: ['@babel/plugin-transform-runtime'],
             },
           },
         ],
