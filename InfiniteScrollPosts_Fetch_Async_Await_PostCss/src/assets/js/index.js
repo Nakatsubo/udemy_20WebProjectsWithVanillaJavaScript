ad4b56786605ad309c970a663b53c66c8cfce2e3
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-04-30 18:49:08 +0900

    Create JavaScript files on the way

--- InfiniteScrollPosts_Fetch_Async_Await_PostCss/src/assets/js/index.js
+++ InfiniteScrollPosts_Fetch_Async_Await_PostCss/src/assets/js/index.js
@@ -1,3 +1,29 @@
 import  '../scss/style.scss';
 
-console.log('Hello, World');
+const postsContainer = document.getElementById('posts-container'),
+      loading        = document.querySelector('.loader'),
+      filter         = document.getElementById('filter');
+
+let limit = 3,
+    page  = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+// Show posts in DOM
+async function showPosts() {
+  const posts = getPosts();
+
+  console.log(posts);
+}
+
+// Show intiail posts
+showPosts();
