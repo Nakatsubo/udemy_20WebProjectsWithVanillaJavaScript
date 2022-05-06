c1ee3f2f69381c16de104d7fad655fa38ab3073f
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-05-06 19:56:06 +0900

    Word Match & Score

--- TypingGame_DOM_Intervals_Event/src/assets/js/index.js
+++ TypingGame_DOM_Intervals_Event/src/assets/js/index.js
@@ -1,3 +1,78 @@
 import  '../scss/style.scss';
 
-console.log('Hello, World');
+const word             = document.getElementById('word'),
+      text             = document.getElementById('text'),
+      scoreEl          = document.getElementById('score'),
+      timeEl           = document.getElementById('time'),
+      endgameEl        = document.getElementById('end-game-container'),
+      settingsBtn      = document.getElementById('settings-btn'),
+      settings         = document.getElementById('settings'),
+      settingsForm     = document.getElementById('settings-form'),
+      difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+// console.log(getRandomWord());
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDOM();
+
+// Event Listers
+text.addEventListener('input', e => {
+  const insertedText = e.target.value;
+  // console.log(insertedText);
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+
+    // Clear
+    e.target.value = '';
+  }
+})
