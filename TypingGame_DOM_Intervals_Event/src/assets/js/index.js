444989e3f622bc4a9514f97c261bb720109897c9
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-05-06 20:21:52 +0900

    Difficulty Setting

--- TypingGame_DOM_Intervals_Event/src/assets/js/index.js
+++ TypingGame_DOM_Intervals_Event/src/assets/js/index.js
@@ -43,6 +43,16 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty = localStorage.getItem('difficulty') !== null 
+  ? localStorage.getItem('difficulty')
+  : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null 
+? localStorage.getItem('difficulty')
+: 'medium';
+
 // Focus on text on start
 text.focus;
 
@@ -94,6 +104,8 @@ function gameOver() {
 addWordToDOM();
 
 // Event Listers
+
+// Typing
 text.addEventListener('input', e => {
   const insertedText = e.target.value;
   // console.log(insertedText);
@@ -105,8 +117,27 @@ text.addEventListener('input', e => {
     // Clear
     e.target.value = '';
 
-    time += 2;
+    // time += 2;
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
 
     updateTime();
   }
+})
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => {
+  settings.classList.toggle('hide');
+})
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  // console.log(difficulty);
+  localStorage.setItem('difficulty', difficulty);
 })
